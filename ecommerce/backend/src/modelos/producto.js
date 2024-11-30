ee48ef71a9e50aeb6524a578c37ba5fdddd69b40
David Alejandro Rodriguez <david.rodriguez@ceiba.com.co>
2024-11-29 23:07:02 -0500

    first commit

---
+++ ecommerce/backend/src/modelos/producto.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const categoria = require('./categoria');
+
+const ProductoSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    precio: {
+        type: Number,
+        required: true,
+    },
+    imagenUrl: {
+        type: String,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        default: 0,
+    },
+    categoria: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Categoria',
+        required: true,
+    },
+    
+});
+module.exports = mongoose.model('Producto', ProductoSchema);
